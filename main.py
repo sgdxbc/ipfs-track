9f17517db5cde76fc94be9fd8967651707989540
sgdxbc <sung@comp.nus.edu.sg>
2025-02-26 12:55:11 +0800

    Check in evaluation results

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import polars as pl
 
 
 def main():
-    print(pl)
+    df = pl.read_csv(
+        "data/*/*.csv",
+        new_columns=["elapsed", "target", "op", "count"],
+        has_header=False,
+    )
+    df_hours = (
+        df.group_by(pl.col("op"), (pl.col("elapsed") // 3600).alias("hour"))
+        .agg(pl.col("count").mean())
+        .sort("hour")
+    )
+    print(df_hours)
+    df_hours.plot.line(x="hour", y="count", color="op")#.save("count.png")
 
 
 if __name__ == "__main__":
